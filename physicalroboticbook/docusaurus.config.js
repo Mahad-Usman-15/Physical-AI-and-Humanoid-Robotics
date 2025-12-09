10c9ebcde1a36946c029d8350f7f02a96c1648a2
Mahad Usman <mahadusman2008@gmail.com>
2025-12-09 23:39:13 +0500

    Change base URL for GitHub Pages deployment

--- physicalroboticbook/docusaurus.config.js
+++ physicalroboticbook/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://Mahad-Usman-15.github.io', // TODO: Replace with your GitHub Pages URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Physical-AI-and-Humanoid-Robotics/', 
+  baseUrl: '/', 
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
